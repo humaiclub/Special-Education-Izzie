7681a726e8df2c9d5b5c7a6ce697ce45e3821e2e
HumAi Club <58257974+humaiclub@users.noreply.github.com>
2020-06-30 08:43:14 -0700

    More progress towards the home page
    Made updates to the logo and header image

---
+++ parts/header.php
@@ -0,0 +1,59 @@
+			<div class="header">
+				<div class="container header-container">
+					<div class="col-lg-6 header-img-section">
+						<img src="assets/images/izzie_header.png">
+					</div>
+					<div class="col-lg-5 offset-lg-1 header-title-section">
+						<p class="header-subtitle">A Partnership Between</p>
+						<h1 class="header-title">Izzie Ai<br />&amp; Human Educators</h1>
+						<p class="header-title-text">Helping solve real world challenges and giving students the opportunity to achieve success!</p>
+						<div class="learn-more-btn-section">
+							<a class="nav-link learn-more-btn btn-invert" href="#services">Learn More</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- STRATEGY SECTION -->
+			<div class="strategy-section">
+				<div class="strategy-section-bg-graphics-section">
+					<img src="assets/images/strategy-section-bg.png">
+				</div>
+				<div class="container strategy-container">
+					<div class="col-lg-3 col-md-6 col-xs-8 offset-xs-2 strategy-card-section">
+						<div class="strategy-card">
+							<div class="strategy-card-icon-section">
+								<img src="assets/images/strategy-1.png">
+							</div>
+							<h2>Strategy</h2>
+							<p>We'll work with you to develop the right strategy</p>
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-6 col-xs-8 offset-xs-2 strategy-card-section">
+						<div class="strategy-card">
+							<div class="strategy-card-icon-section">
+								<img src="assets/images/strategy-2.png">
+							</div>
+							<h2>Automation</h2>
+							<p>Work synchronization and automated services</p>
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-6 col-xs-8 offset-xs-2 strategy-card-section">
+						<div class="strategy-card">
+							<div class="strategy-card-icon-section">
+								<img src="assets/images/strategy-3.png">
+							</div>
+							<h2>Deployment</h2>
+							<p>Dedicated servers to deploy and test applications</p>
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-6 col-xs-8 offset-xs-2 strategy-card-section">
+						<div class="strategy-card">
+							<div class="strategy-card-icon-section">
+								<img src="assets/images/strategy-4.png">
+							</div>
+							<h2>Education</h2>
+							<p>Stay informed on all our tools and technologies</p>
+						</div>
+					</div>
+				</div>
+			</div>
